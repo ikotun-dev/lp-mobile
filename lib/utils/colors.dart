1857d16ebebd970103cc0cf368fc74a8f85877e5
Emmy Wales <yourgitemail@example.com>
2023-07-20 21:41:18 +0100

    third commit

---
+++ lib/utils/colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+class AppColor {
+  static Color primary1 = const Color(0XFF007FE0);
+  static Color primary2 = const Color(0XFF33A7FF);
+  static Color primary3 = const Color(0XFF99D3FF);
+  static Color txt1 = const Color(0XFF1C140D);
+  static Color txt2 = const Color(0XFF463220);
+  static Color txt3 = const Color(0XFFAE7C51);
+  static Color txt4 = const Color(0XFFCEB197);
+
+  static Color extra = const Color(0XFF0C1D13);
+  static Color extra2 = const Color(0XFF1E482F);
+  static Color extra3 = const Color(0XFF3C905F);
+  static Color extra4 = const Color(0XFF6FC392);
+  
+
+  
+}
---
+++ lib/utils/text.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class BaseText extends StatelessWidget {
+  final String text;
+  final FontWeight? weight;
+  final double? size;
+  final Color? color;
+  final TextAlign? alignText;
+  final FontStyle? fontStyle;
+
+  const BaseText({
+    super.key,
+    required this.text,
+    this.weight,
+    this.size,
+    this.color,
+    this.alignText,
+    this.fontStyle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      textAlign: alignText ?? TextAlign.start,
+      overflow: TextOverflow.fade,
+      style: GoogleFonts.inter(
+            color: color,
+            fontWeight: weight,
+            fontSize: size,
+            fontStyle: fontStyle,
+          ),
+    );
+  }
+}
